63a533e91d07dabfd2895604007f9f151c64b002
William Taylor <william.taylor.1707@gmail.com>
2018-12-16 23:07:05 +0100

    Adding test

---
+++ bunnet-server/test/java/com/ms/bunnet/BasicTest.java
@@ -0,0 +1,11 @@
+package com.ms.bunnet;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class BasicTest {
+    @Test
+    public void test() {
+        assertEquals("Dummy test", 0, 0);
+    }
+}
